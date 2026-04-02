dd4e4c38a07e1f3ee45c9b835edc5f7aef4b321e
bluehkm <bluehakim@protonmail.com>
2026-04-02 23:03:07 +0200

    pwConf

--- playwright.config.js
+++ playwright.config.js
@@ -1,42 +1,58 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
 
+import * as dotenv from 'dotenv'
+dotenv.config();
 
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
 export default defineConfig({
   testDir: './tests',
+  /* Run tests in files in parallel */
   fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
   retries: 1,
-  workers: 5,
+  /* Opt out of parallel tests on CI. */
+  workers: 1,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://localhost:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
     screenshot: 'on-first-failure',
-    video: 'on-first-retry',
+    //video: 'retain-on-failure'
+    //video: {mode: 'retain-on-failure', size: {width: 1920, height: 1080} },
+    headless: false,
   },
 
+  /* Configure projects for major browsers */
   projects: [
-
 
     {
       name: 'chromium',
       use: { 
         ...devices['Desktop Chrome'],
         viewport: {width: 1900, height: 1080} 
-      },
+       },
     },
-
-  /*
     
+    
+  /* 
   {
-      name: 'Google Chrome',
+      name: 'chromium',
       use: { 
         ...devices['Desktop Chrome'],
-        channel: 'chrome',
         viewport: {width: 1900, height: 1080} 
-      },
+       },
     },
- 
+
     {
       name: 'firefox',
       use: { 
@@ -52,8 +68,26 @@ export default defineConfig({
         viewport: {width: 1850, height: 1000}  
       },
     },
+
+     {
+      name: 'Google Chrome',
+      use: { 
+        ...devices['Desktop Chrome'],
+        channel: 'chrome',
+        viewport: {width: 1900, height: 1080} 
+       },
+    },
+
+    {
+      name: 'Microsoft Edge',
+      use: { 
+        ...devices['Desktop Edge'],
+        channel: 'msedge',
+        viewport: {width: 1900, height: 1080} 
+       },
+    },
 */
-   
+
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
